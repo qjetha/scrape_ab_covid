647ddfa50696395abfe1afcdda454d6a81cb6d86
qjetha <qjetha@gmail.com>
2021-01-29 21:39:25 -0700

    lets try it

--- scrape_alberta_heroku.py
+++ scrape_alberta_heroku.py
@@ -5,9 +5,9 @@ from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-
 
 class RegionData:
 
@@ -40,7 +40,6 @@ class RegionData:
 
 
 def scrape_alberta():
-
 	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 	alberta_listings = r"https://www.alberta.ca/maps/covid-19-status-map.htm"
